0f3e8e5b9fb24357dca49c28522e2fd5eff45473
SKalt <kalt.steven@gmail.com>
2018-06-17 19:36:11 -0400

    Added css import example

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,7 @@
 import hello from './hello';
 import {world} from './world';
 import json from './you_can_import';
-alert('foo.js');
+import './you_can_import.css'
+alert('from main.js');
 document.getElementById('main').innerHTML = `${hello} ${world}`;
 alert(JSON.stringify(json));
