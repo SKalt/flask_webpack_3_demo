6231707cc8b05daefc1aa5ac8277b330a0f174eb
SKalt <kalt.steven@gmail.com>
2018-06-17 12:04:27 -0400

    Registered frontend/dist as a static folder in the flask app

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,21 @@
 #! /usr/bin/env python3
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, Blueprint
 from flask_webpack import Webpack
 
 
 webpack = Webpack()
 app = Flask(__name__)
 # configure the flask application
-app.config['WEBPACK_MANIFEST_PATH'] = os.path.join(
+dist_path = os.path.join(
     os.path.dirname(os.path.realpath(__file__)), '..',  # the project root
-    'frontend', 'dist', 'manifest.json'
+    'frontend', 'dist'
 )
 
+app.config['WEBPACK_MANIFEST_PATH'] = os.path.join(dist_path, 'manifest.json')
+
+dist = Blueprint('dist', __name__, static_folder=dist_path)
+app.register_blueprint(dist)
 
 @app.route('/')
 def hello_world():
